fa463ed103ba29f2baf17d0e0d0b5daa7dbd6460
Kris Tina <kristyguerrero20@gmail.com>
2021-07-15 23:18:59 -0500

    created html and added containers

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-//Example that Henry wrote 
+//Example that Henry wrote
 function createHTML(allEmployees) {
-    let allCards = " ";
-    allEmployees.forEach(employee => {
-        allCards +- renderCards(employee);
-    });
+  let allCards = " ";
+  allEmployees.forEach((employee) => {
+    allCards + -renderCards(employee);
+  });
 
-    return <html>
-        <body>
-            ${renderCards()}
-        </body>
+  return (
+    <html>
+      <body>${renderCards()}</body>
     </html>
+  );
 }
 
-function renderCard (empolyee) {
-    switch(employee.getRole()) {
-        case "engineer""
-        return `<div>
-        <h2>Hi, I'm ${employee.getName()}</h2>
-        <h4>I'm an engineer!</h4>
-        </div>`;
-    }
-}
+// function renderCard (empolyee) {
+//     switch(employee.getRole()) {
+//         case "engineer""
+//         return `<div>
+//         <h2>Hi, I'm ${employee.getName()}</h2>
+//         <h4>I'm an engineer!</h4>
+//         </div>`;
+//     }
+// }
