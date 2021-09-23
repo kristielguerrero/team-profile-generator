2310178444b8721b55ba11bcdf0bb357847fec3d
Kris Tina <kristyguerrero20@gmail.com>
2021-09-23 18:33:33 -0500

    starting the index.js by creating the addEMployee function

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
+var teamEmployees = [];
+
+var init = () => {
+  addEmployee();
+};
+
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "teamRole",
+        message: "What team member are you adding?",
+        choices: ["Manager", "Engineer", "Intern", "None"],
+      },
+    ])
+    .then((choices) => {
+      switch (choices.teamRole) {
+        case "Manager":
+          addManager;
+      }
+    });
+}
+
+function addEmployee() {
+  inquirer.prompt(add);
+}
