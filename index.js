8fc62091cf449198e038c0a97c72a9e0664caf95
Kris Tina <kristyguerrero20@gmail.com>
2021-09-23 18:15:38 -0500

    added all the requirements in the index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-//Example that Henry wrote
-// function createHTML(allEmployees) {
-//   let allCards = " ";
-//   allEmployees.forEach((employee) => {
-//     allCards + -renderCards(employee);
-//   });
-
-//   return (
-//     <html>
-//       <body>${renderCards()}</body>
-//     </html>
-//   );
-// }
-
-// function renderCard (empolyee) {
-//     switch(employee.getRole()) {
-//         case "engineer""
-//         return `<div>
-//         <h2>Hi, I'm ${employee.getName()}</h2>
-//         <h4>I'm an engineer!</h4>
-//         </div>`;
-//     }
-// }
+const inquirer = require("inquirer");
+const fs = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
